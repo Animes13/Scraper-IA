0a88fd38d393f6acd0080f90762afcc907d149d3
Animes13 <142902852+Animes13@users.noreply.github.com>
2026-01-15 11:44:42 -0300

    Update validator.py

--- utils/validator.py
+++ utils/validator.py
@@ -4,6 +4,12 @@
 from bs4 import BeautifulSoup
 
 
-def selector_has_results(html, selector):
-    soup = BeautifulSoup(html, "html.parser")
-    return bool(soup.select(selector))
+def selector_has_results(html: str, selector: str, min_results: int = 1) -> bool:
+    """
+    Verifica se um seletor CSS retorna resultados suficientes
+    """
+    try:
+        soup = BeautifulSoup(html, "html.parser")
+        return len(soup.select(selector)) >= min_results
+    except Exception:
+        return False
